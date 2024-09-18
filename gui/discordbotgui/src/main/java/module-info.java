49cbcda3f92f3559caf228b4ca9350a7c379f221
=jthorp07 <=>
2024-09-18 15:39:02 -0400

    Removed unnecessary files from template

--- gui/discordbotgui/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.thorpjj.discord.bot.discordbotgui.discordbotgui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.thorpjj.discord.bot.discordbotgui.discordbotgui to javafx.fxml;
-    exports com.thorpjj.discord.bot.discordbotgui.discordbotgui;
-}
