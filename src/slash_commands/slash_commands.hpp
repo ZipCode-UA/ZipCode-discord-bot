2ccd7684e3ab94a205e5d38a72512ec3046881c9
Jack Thorp <jackthorp07@gmail.com>
2025-02-17 12:38:16 -0500

    Add slash_commands.hpp

---
+++ src/slash_commands/slash_commands.hpp
@@ -0,0 +1,19 @@
+/**
+ * slash_commands.hpp
+ * 
+ * Include file for bot slash commands
+ */
+
+#ifndef SLASH_COMMANDS_HPP
+#define SLASH_COMMANDS_HPP
+
+#include <dpp/cluster.h>
+
+/**
+ * Registers all slash commands globally
+ * 
+ * @param[in, out] bot Bot instance
+ */
+int registerSlashCommands(dpp::cluster& bot);
+
+#endif
