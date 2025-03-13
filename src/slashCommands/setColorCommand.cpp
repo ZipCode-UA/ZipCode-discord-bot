1b8833f0e21e43f60a2a1fd5626757505e072f02
Riley Spires <rjs243@uakron.edu>
2025-03-13 18:18:01 -0400

    Working /set-color {hex} command

--- src/slashCommands/setColorCommand.cpp
+++ src/slashCommands/setColorCommand.cpp
@@ -7,6 +7,7 @@
 namespace SetColorCommand {
     void setColorCommand(const dpp::slashcommand_t &event) {
         dpp::cluster *bot = ZipCode::DiscordBot::getInstance()->getCluster();
+        const std::string GUILD_ID = std::getenv("GUILD_ID");
 
         if (event.command.get_command_name() != "set-color") return;
 
@@ -19,12 +20,12 @@ namespace SetColorCommand {
         dpp::guild_member user = dpp::find_guild_member(event.command.guild_id,
                                                          event.command.usr.id);
 
-        dpp::role *role = nullptr;
+        dpp::role *rolePtr = nullptr;
 
         for (const auto &roleSnowflake : guild.roles) {
-            role = dpp::find_role(roleSnowflake);
-            if (role->name != user.user_id.str()) {
-                role = nullptr;
+            rolePtr = dpp::find_role(roleSnowflake);
+            if (rolePtr->name != user.user_id.str()) {
+                rolePtr = nullptr;
                 continue;
             }
 
@@ -33,22 +34,66 @@ namespace SetColorCommand {
 
         bool roleExisted = true;
         
-        if (role == nullptr) {
+        if (rolePtr == nullptr) {
             roleExisted = false;
-            role = new dpp::role();
+            rolePtr = new dpp::role();
+        }
+
+        if (colorStr.starts_with('#')) {
+            colorStr = colorStr.substr(1);
+        }
+
+        if (colorStr.length() != 6) {
+            event.reply("Error: Invalid length for hex code. Do not include an alpha channel");
+            return;
         }
         
         uint32_t color = std::stoi(colorStr, nullptr, 16);
 
-        role->set_color(color);
+        rolePtr->set_color(color);
+
+        dpp::role role;
+
+        bool wait = true;
 
         if (roleExisted) {
-            bot->role_edit(*role);
+            bot->role_edit(*rolePtr);
         } else {
-            role->set_guild_id(guild.id);
-            bot->role_create(*role);
+            rolePtr->set_guild_id(guild.id);
+            rolePtr->set_name(user.user_id.str());
+            bot->role_create(*rolePtr, [&role, &wait](const dpp::confirmation_callback_t &ev) {
+                if (!ev.is_error()) {
+                    role = std::get<dpp::role>(ev.value);
+                }
+                wait = false;
+            });
+
+            while (wait) {}
         }
+
+        rolePtr = &role;
         
-        user.add_role(role->id);
+        user = user.add_role(rolePtr->id);
+
+        std::cout << "USER: " << user.get_nickname() << std::endl;
+        for (const auto &r : user.get_roles()) {
+            std::cout << r.str() << std::endl;
+        }
+
+        std::string err = "";
+
+        bot->guild_edit_member(user, [&err](const dpp::confirmation_callback_t &ev) {
+            if (ev.is_error()) {
+                const dpp::cluster *bot = ev.bot;
+
+                err = ev.get_error().human_readable;
+            }
+        });
+
+        if (err != "") {
+            event.reply("Failed to update your color because: " + err);
+        } else {
+            event.reply("Your color has been updated!");
+        }
     }
 }
